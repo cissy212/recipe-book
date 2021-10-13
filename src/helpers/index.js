7494e04321175fba283cb1425b5201dd7bf5327f
Maria Cecilia Canteros <Maria.Canteros@endava.com>
2021-10-12 22:51:39 -0300

    data sample in the home

---
+++ src/helpers/index.js
@@ -0,0 +1,3 @@
+export const isLoading = (status) => Boolean(status === "loading");
+
+export const hasError = (status) => Boolean(status === "error");
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import * as actions from "../actions";
+import * as helper from "../helpers";
 
 const Home = () => {
   const [inputSearch, setInputSearch] = useState("");
+  const dispatch = useDispatch();
+  const { recipesData, statusRecipesData } = useSelector(
+    (state) => state.mainReducer
+  );
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    dispatch(actions.getRecipeSummary());
+  };
+
+  const loading = helper.isLoading(statusRecipesData);
+  const error = helper.hasError(statusRecipesData);
 
   return (
     <div>
@@ -23,10 +41,23 @@ const Home = () => {
         <Link to="/scores">Ver Valoraciones</Link>
       </div>
       <div>
-        <table>
+        <table border={1}>
           <thead>
-            <tr></tr>
+            <tr>
+              <th>Receta</th>
+              <th>Puntaje</th>
+            </tr>
           </thead>
+          <tbody>
+            {recipesData.map((item) => (
+              <tr>
+                <td>
+                  <Link to={`/recipe/${item.id}`}>{item.name}</Link>
+                </td>
+                <td>{item.score}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
